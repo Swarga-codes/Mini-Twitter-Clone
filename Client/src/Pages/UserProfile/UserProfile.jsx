7c1946dc5918094886484046bb59636c450b578f
Swarga-codes <sd3526@srmist.edu.in>
2023-09-14 01:33:20 +0530

    fixed follow unfollow api

--- Client/src/Pages/UserProfile/UserProfile.jsx
+++ Client/src/Pages/UserProfile/UserProfile.jsx
@@ -36,18 +36,17 @@ function UserProfile() {
 getAllTweets()
       },[])
   return (
-    <div className='bg-black w-[100%] h-[100vh] text-white p-6 ml-[16rem]'>
+    <div className='bg-black w-full h-[100vh] text-white p-6 ml-[16rem]'>
     <div className='mt-10'>
     <img
     className="inline-block h-36 w-36 rounded-full"
     src={user?.profilePic}
     alt="profile pic"
   />
-  <div className="flex">
+ 
   <h1 className="mt-10 font-bold text-2xl">{user?.userName}</h1>
-  <button className='flex items-center px-10 py-[0.5rem]  ml-96 bg-blue-400 text-white font-bold text-xl rounded-full'>
+  <button className='flex mt-5 items-center px-10 py-[0.5rem] bg-blue-400 text-white font-bold text-xl rounded-full'>
   Follow</button>
-  </div>
   <p className='mt-5 font-semibold text-lg'>Joined on 14th October 2022</p>
   <p className='mt-5 font-semibold text-lg'><b>{user?.following?.length} </b>Following &nbsp; &nbsp; <b>{user?.followers?.length} </b>Followers</p>
   </div>
--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -18,20 +18,38 @@ const getUserData=async(req,res)=>{
 //Follow user
  const followUser=async(req,res)=>{
     const {userId}=req.body
+    try{
+    if(!userId) return res.status(400).json({error:'User id cannot be empty'})
+    if(userId==req.user._id) return res.status(422).json({error:'User cannot follow self account'})
+    const isFollowing=await USER.findById(req.user._id)
+    if(isFollowing.following.includes(userId)) return res.status(409).json({error:'User already followed'})
     const updateFollowing=await USER.findByIdAndUpdate(req.user._id,{$push:{following:userId}},{new:true})
     if(!updateFollowing) return res.status(500).json({error:'Could not update following'})
     const updateFollowers=await USER.findByIdAndUpdate(userId,{$push:{followers:req.user._id}},{new:true})
     if(!updateFollowers) return res.status(500).json({error:'Could not update followers'})
 return res.status(200).json({message:'User followed successfully'})
+    }
+    catch(error){
+        return res.status(500).json({error:'Internal Server Error'})
+    }
 }
 
 //Unfollow user
 const unfollowUser=async(req,res)=>{
     const {userId}=req.body
+    try{
+    if(!userId) return res.status(400).json({error:'User id cannot be empty'})
+    if(userId==req.user._id) return res.status(422).json({error:'User cannot unfollow self account'})
+    const isFollowing=await USER.findById(req.user._id)
+    if(!isFollowing.following.includes(userId)) return res.status(409).json({error:'User already unfollowed'})
     const updateFollowing=await USER.findByIdAndUpdate(req.user._id,{$pull:{following:userId}},{new:true})
     if(!updateFollowing) return res.status(500).json({error:'Could not update following'})
     const updateFollowers=await USER.findByIdAndUpdate(userId,{$pull:{followers:req.user._id}},{new:true})
     if(!updateFollowers) return res.status(500).json({error:'Could not update followers'})
-return res.status(200).json({message:'User followed successfully'})
+return res.status(200).json({message:'User unfollowed successfully'})
+    }
+    catch(error){
+        return res.status(500).json({error:'Internal server error'})
+    }
 }
 module.exports={getUserData,followUser,unfollowUser}
