a70dd6ec52eca5a2535492f392e036a0ab4d816d
Swarga-codes <sd3526@srmist.edu.in>
2023-09-15 04:29:29 +0530

    added frontend domain to cors

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const cors=require('cors')
 app.use(express.json())
 app.use(cors({
     origin:["http://localhost:5173",
-    "http://127.0.0.1:5173","https://mini-twitter-clone-production.up.railway.app"]
+    "http://127.0.0.1:5173","https://mini-twitter-clone-six.vercel.app"]
     ,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
